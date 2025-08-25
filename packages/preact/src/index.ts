2c60fb3d727465898c58b38ee7546b1045a6b68a
Umair Siddique <umairsiddique@gmail.com>
2025-08-25 22:43:22 +0400

    Added preact.

---
+++ packages/preact/src/index.ts
@@ -0,0 +1,16 @@
+// Unlayer Preact Elements
+// This package provides Preact components for building emails, pages, and documents
+// Components will be generated and placed here through an automated process
+
+// Basic exports - will be populated by automation
+export const version = '0.0.0';
+
+// Placeholder for future component exports
+// export { Button } from './components/Button';
+// export { Row } from './components/Row';
+// export { Column } from './components/Column';
+// export { Image } from './components/Image';
+// export { Text } from './components/Text';
+// export { Form } from './components/Form';
+// export { Input } from './components/Input';
+// export { Select } from './components/Select';
