1c1b55b135c68a316aa52cff53fae865df8ad269
Umair Siddique <umairsiddique@gmail.com>
2025-08-27 23:17:27 +0400

    Update main.ts

--- packages/vue/.storybook/main.ts
+++ packages/vue/.storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
+import vue from '@vitejs/plugin-vue';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|vue)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
@@ -9,6 +10,11 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.plugins = config.plugins || [];
+    config.plugins.push(vue());
+    return config;
   },
 };
 
